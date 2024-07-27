af454790822897cfbf0e096e33d2c08b01327021
akashdeep <akashdeep@akashdeeps-MacBook-Air.local>
2024-07-27 15:12:34 +0530

    graph : Dijkastra and it's application of neighbout

--- graph/NeighbourWithThreshold.java
+++ graph/NeighbourWithThreshold.java
@@ -1,31 +1,65 @@
 import java.util.stream.IntStream;
 
-public class Dijkastra {
+public class NeighbourWithThreshold {
+    int globalThreshold = 0;
     public static void main(String[] args) {
-        int source = 0;
-        int[][] graph = {
-                { 0, 3, 0, 0, 0 },
-                { 3, 0, 1, 4, 0 },
-                { 0, 1, 0, 1, 1 },
-                { 0, 4, 1, 0, 2 },
-                { 0, 0, 1, 2, 0 }
+        int n = 5;
+        int[][] edges = {
+            {0, 1, 2},
+            {0, 4, 8},
+            {1, 2, 3},
+            {1, 4, 2},
+            {2, 3, 1},
+            {3, 4, 1}
         };
-        Dijkastra dijkastra = new Dijkastra();
-
-        int[] dist = dijkastra.dijkastra(graph, source);
-        for (int i = 0; i < dist.length; i++) {
-            System.out.println(source + " to " + i + " = " + dist[i]);
-        }
-        System.out.println("done");
+        int threshold = 2;
+        NeighbourWithThreshold neighbourWithThreshold = new NeighbourWithThreshold();
+        neighbourWithThreshold.globalThreshold = threshold;
+        int smallestCity = neighbourWithThreshold.findTheCity(n, edges, threshold);
+        System.out.println("Smallest city (largest number if conflict) " + smallestCity);
     }
+
+    public int[][] buildGraph(int[][] edges, int n){
+        int[][] graph = new int[n][n];
+        for (int[] edge : edges) {
+            graph[edge[0]][edge[1]] = edge[2];
+            graph[edge[1]][edge[0]] = edge[2];
+        }
+        return graph;
+    }
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int smallestCity = Integer.MAX_VALUE;
+        int smallestCityIdx = -1;
+        int[][] graph = buildGraph(edges, n); 
+        for (int i = 0; i < n; i++) {
+            int[] dist = dijkastra(graph, i);
+            for (int k = 0; k < dist.length; k++) {
+                System.out.println(i + " to " + k + " = " + dist[k]);
+            }
+            int cityCount = 0;
+            for (int j = 0; j < dist.length; j++) {
+                if(i != j && dist[j] <= distanceThreshold) {
+                    cityCount++;
+                }
+            }
+            System.out.println("cityCount " + cityCount);
+
+            if(cityCount <= smallestCity){
+                smallestCity = cityCount;
+                smallestCityIdx = i;
+            }
+        }
+        return smallestCityIdx;
+    }
+
 
     // Method to find the vertex with the minimum distance value, from
     // the set of vertices not yet included in the shortest path tree
     public int minDistanceVertex(boolean[] sptSet, int[] dist) {
         int vertU = -1;
-        int min = Integer.MAX_VALUE;
+        int min = globalThreshold;
         for (int i = 0; i < dist.length; i++) {
-            if (!sptSet[i] && dist[i] < min) {
+            if (!sptSet[i] && dist[i] <= min) {
                 min = dist[i];
                 vertU = i;
             }
