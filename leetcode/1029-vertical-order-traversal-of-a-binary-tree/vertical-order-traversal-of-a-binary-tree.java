d2da1e57b6c41c8badc9cea9812ae04bec3b45a2
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-25 20:07:02 +0200

    Time: 4 ms (62.46%) | Memory: 44.2 MB (41.34%) - LeetSync

--- leetcode/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ leetcode/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -13,55 +13,80 @@
  *     }
  * }
  */
-class Solution {
-    class Trio {
-        TreeNode node;
-        int x;
-        int y;
-        Trio(TreeNode node, int x, int y) {
-            this.node = node;
-            this.x = x;
-            this.y = y;
-        }
-    }
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        int SIZE = 2000;
-        List[][] index = new List[SIZE][SIZE / 2];
-        index[0][0] = new ArrayList<>();
-        Queue<Trio> queue = new LinkedList<>();
-        queue.add(new Trio(root, SIZE / 2, 0));
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i = 0; i < size; i++) {
-                Trio t = queue.remove();
-                List<Integer> tempList = null;
-                if(index[t.x][t.y] == null) {
-                    index[t.x][t.y] = new ArrayList<>();
-                }
-                tempList = index[t.x][t.y];
-                
-                tempList.add(t.node.val);
-                
-                if(t.node.left != null)
-                    queue.add(new Trio(t.node.left, t.x - 1, t.y + 1));
-                if(t.node.right != null)
-                    queue.add(new Trio(t.node.right, t.x + 1, t.y + 1));
 
-            }
+ // Create class tuple to store the node and coordinates.
+ class Tuple{
+    TreeNode node;
+    int row;
+    int col;
+    // Constructor for tuple.
+    public Tuple(TreeNode _node, int _row, int _col){
+        node = _node;
+        row = _row;
+        col = _col;
+    }
+ }
+class Solution {
+
+    // We perform Level order trversal to get the output....
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        // We need a treemap to store the vertical values(columns) and PriorityQueue to store the node values in increasing order.
+        // (x,y,node)
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
+
+        // Create a queue for instering each node with respective row(x), column(y) values during iteration.
+        // Initially coordinates of node are...(node,x->(0),y->(0))
+        Queue<Tuple> q = new LinkedList<Tuple>();
+
+        // Insert the tuple
+        q.add(new Tuple(root,0,0));
+
+        // Loop untill queue is empty.
+        while(!q.isEmpty()){
+
+            // Pop the tuple from stack.
+            Tuple tuple = q.poll();
+
+            // Initialize the values inside the tuple.
+            TreeNode node = tuple.node;
+            int x = tuple.row;
+            int y = tuple.col;
+
+            // Insert the values into the treemap.
+
+            // x - > vertical coordinate --> check example test cases.
+            if(!map.containsKey(x)) map.put(x,new TreeMap<>());
+
+            // y - > horizontal coordinate --> check example test cases.
+            if(!map.get(x).containsKey(y)) map.get(x).put(y,new PriorityQueue<>());
+
+            // Finally insert node value (!!!not node!!!) into map inside PriorityQueue.
+            map.get(x).get(y).add(node.val);
+
+            // Check is there exists a left or right node to the node present in the queue.
+            // If present, then add it to the queue.
+            if(node.left!=null) q.add(new Tuple(node.left,x-1,y+1));
+            if(node.right!=null) q.add(new Tuple(node.right, x+1,y+1));
         }
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i= 0; i < SIZE; i++) {
-            List<Integer> vertList = new ArrayList<>();
-            for(int j = 0; j < SIZE / 2; j++) {
-                if(index[i][j] != null) {
-                    index[i][j].sort(Comparator.naturalOrder());
-                    vertList.addAll(index[i][j]);
+
+        // Create a List Of List to store the list of node values.
+        List<List<Integer>> list = new ArrayList<>();
+
+        // Loop through the map and add the values.
+        // x - > key, (y, nodes) -> values.
+        for(TreeMap<Integer,PriorityQueue<Integer>> yn : map.values()){
+            // Create a sublist to store node values in each vertical.
+            list.add(new ArrayList<>());
+
+            // Now iterate in the PriorityQueue.
+            for(PriorityQueue<Integer> nodes : yn.values()){
+                // Add node into the sublist from 
+                while(!nodes.isEmpty()){
+                    list.get(list.size()-1).add(nodes.poll());
                 }
             }
-            if(!vertList.isEmpty()) {
-                result.add(vertList);
-            }
-        }
-        return result;
+        }return list;
     }
 }
