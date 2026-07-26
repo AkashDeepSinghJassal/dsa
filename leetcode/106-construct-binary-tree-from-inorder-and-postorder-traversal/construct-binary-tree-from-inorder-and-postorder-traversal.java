055620e0eecbcd2b00b5a546edf612eddd6edcf8
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-26 12:54:17 +0200

    Time: 25 ms (3.84%) | Memory: 53.2 MB (9.11%) - LeetSync

---
+++ leetcode/106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        // build root
+        int i = 0;
+        int j = inorder.length - 1;
+        return build(inorder, postorder, i, j, i, j);
+    }
+
+    public TreeNode build(int[] inorder, int[] postorder, int i, int j, int m, int n) {
+        if(j - i < 0){
+            return null;
+        }
+        int currRootVal = postorder[n];
+        TreeNode root = new TreeNode(currRootVal);
+        if(i ==j ){
+            return root;
+        }
+        // find index of curr root in inorder
+        int idx = -1;
+        for(int a = i; a <= j; a++) {
+            if(inorder[a] == currRootVal) {
+                idx = a;
+                break;
+            }
+        }
+        System.out.println("index : " + idx + " at i : " + i + " j :  " + j + " root val " + currRootVal + " m : " + m + " n : " + n);
+        int shift = j - idx + 1;
+        // build left sub tree
+        // build right sub tree
+        // add pointers
+        root.left = build(inorder, postorder, i, idx - 1, m, n - shift);
+        root.right = build(inorder, postorder, idx + 1, j, shift + 1, n - 1);
+        return root;
+    }
+}
