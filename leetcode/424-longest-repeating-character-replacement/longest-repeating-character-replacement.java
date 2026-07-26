9bca60a400cc3e8db4501e323d90bbb63cba115e
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-26 22:24:19 +0200

    Time: 332 ms (5.01%) | Memory: 53.2 MB (7.43%) - LeetSync

---
+++ leetcode/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l,r;
+        l = 0;
+        r = 0;
+
+        // take a map of count of chars
+        // max of -> if length of window - max freq <= k then len.window
+        int hash[] = new int[26];
+        int maxFreq = 0;
+        int maxLen = 0;
+        while(r < s.length()) {
+            char c = s.charAt(r);
+            hash[c - 'A']++;
+            maxFreq = Math.max(maxFreq, hash[c - 'A']);
+            while(l <= r && (r - l + 1) - maxFreq > k) {
+                hash[s.charAt(l) - 'A']--;
+                System.out.println("hash at l " + s.charAt(l)  + " r " + s.charAt(r) + " is " + hash[s.charAt(l) - 'A'] + " and max freq");
+                l++;
+                maxFreq = findMaxFreq(hash);
+            }
+            // find max Freq
+            maxLen = Math.max(maxLen, r - l + 1);
+            r++;
+            
+        }
+        return maxLen;
+    }
+    int findMaxFreq(int[] hash) {
+        int max = 0;
+        for(int i = 0; i < hash.length; i++) {
+            max = Math.max(max, hash[i]);
+        }
+        return max;
+    }
+}
