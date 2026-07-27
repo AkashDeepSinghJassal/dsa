c0cb6b3eca7b6029e9b4a7199cfbb366ab03c965
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-27 22:56:48 +0200

    Time: 24 ms (76.66%) | Memory: 48.9 MB (46.10%) - LeetSync

--- leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -11,9 +11,9 @@ class Solution {
         int prefixSum = 0;
         for(int i = 0; i < nums.length; i++){
             prefixSum += nums[i];
-            // if(map.containsKey(prefixSum - k)) {
-                count += map.getOrDefault(prefixSum - k, 0);
-            // }
+            if(map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
             map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
