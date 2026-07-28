57ec4a7f773200709c2aa52024a21b3c6893b057
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-28 17:19:06 +0200

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.33%) - LeetSync

--- leetcode/70-climbing-stairs/climbing-stairs.java
+++ leetcode/70-climbing-stairs/climbing-stairs.java
@@ -1,7 +1,8 @@
 class Solution {
     int[] hash = new int[46];
     public int climbStairs(int n) {
-        return climb(0, n);
+        // return climb(0, n);
+        return climbTabular(n);
     }
     int climb(int currLev, int n) {
         int count = 0;
@@ -21,5 +22,20 @@ class Solution {
         // System.out.println("curr lev " + currLev + " count " + count);
         hash[currLev] = count;
         return count;
+    }
+    int climbTabular(int n ) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        int last = 2;// step 2
+        int secondLast = 1;
+        int curr = 0;
+        for(int i = 3; i <= n; i++) {
+            curr = last + secondLast;
+            secondLast = last;
+            last = curr;
+        }
+        return curr;
     }
 }
