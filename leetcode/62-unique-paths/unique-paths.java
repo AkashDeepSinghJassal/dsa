517daa0b66e8a09dafae9e7b0c6b32f5543bdc93
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-30 19:27:10 +0200

    Time: 0 ms (100.00%) | Memory: 42.4 MB (13.10%) - LeetSync

--- leetcode/62-unique-paths/unique-paths.java
+++ leetcode/62-unique-paths/unique-paths.java
@@ -12,7 +12,10 @@ class Solution {
         [m][0] = 1;
 
      */
-    public int uniquePaths(int m, int n) {
+     public int uniquePaths(int m, int n) {
+        return uniquePathsSpaceOp(m, n);
+    }
+    public int uniquePathsIterative(int m, int n) {
         int dp[][] = new int[m][n];
         // base 
         for(int i = 0; i < n; i++) {
@@ -27,5 +30,22 @@ class Solution {
             }
         }
         return dp[m - 1][n - 1];
+    }
+    public int uniquePathsSpaceOp(int m, int n) {
+        int dp[][] = new int[2][n];
+        // base 
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for(int i = 0; i < 2; i++) {
+            dp[i][0] = 1;
+        }
+        int i;
+        for(i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i % 2][j] = dp[(i - 1) % 2][j] + dp[i % 2][j - 1];
+            }
+        }
+        return dp[(i - 1) % 2][n - 1];
     }
 }
