f10345ded67330657e9a3246202a6cf44b8b48c9
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-30 16:31:45 +0200

    Time: 0 ms (100.00%) | Memory: 42.4 MB (95.15%) - LeetSync

--- leetcode/198-house-robber/house-robber.java
+++ leetcode/198-house-robber/house-robber.java
@@ -1,5 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
+        return robNoSpace(nums);
+    }
+    public int robDp(int[] nums) {
         if(nums.length == 1) {
             return nums[0];
         }
@@ -12,5 +15,20 @@ class Solution {
             
         }
         return dp[nums.length];
+    }
+    public int robNoSpace(int[] nums) {
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        int prev = nums[0];
+        int prev2 = 0;
+        int curr = 0;
+        for(int i = 1; i < nums.length; i++) {
+            curr = Math.max(prev2 + nums[i], prev);
+            prev2 = prev;
+            prev = curr;
+
+        }
+        return curr;
     }
 }
