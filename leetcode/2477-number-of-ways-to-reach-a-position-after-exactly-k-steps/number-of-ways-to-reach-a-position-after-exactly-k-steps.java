c8529cf4877f526deb50199e96b012f3488dbb13
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-07-31 02:28:10 +0200

    Time: 94 ms (38.62%) | Memory: 42.7 MB (85.52%) - LeetSync

---
+++ leetcode/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -0,0 +1,82 @@
+class Solution {
+    /**
+        difference in odd steps required k to be odd, vice versa with even
+
+        Get all possible outcomes
+        - 1 -> 2 -> 3 -> 2.
+        - 1 -> 2 -> 1 -> 2.
+        - 1 -> 0 -> 1 -> 2
+        [index][steps_left]
+
+        f[i][k] = f[i - 1][k+1] + f[i + 1][k+1]
+
+        start pos = 2 end pos = 4 k = 2
+        f[startpos][2] = f[start pos - 1][1] + f[start pso + 1][1]
+        size is 2k , start pos is center 
+        for i - > k
+
+        start pos = 0 
+        end pos = difference
+        base case ??
+        [2][3] = [1][2] [3][2]
+
+
+        diff <= k
+
+
+        pos in K number of steps
+
+        f(pos, k) {
+
+            if(k == k && pos == pos) {
+            return 1}
+            return f(pos - 1, k + 1) + ff(pos + 1, k + 1);
+        }
+
+     */
+    int dp[][];
+    public int numberOfWaysIterative(int startPos, int endPos, int k) {
+        int pos = Math.abs(startPos - endPos);
+        int mod = 1000000007;
+        if(pos > k || (pos % 2) != (k % 2)) {
+            return 0;
+        }
+        int[][] arr = new int[2 * k + 1][2];
+        arr[k ][0] = 1;
+        int i;
+        for(i = 1; i <= k; i++) {
+            for(int j = 0; j <= 2 * k; j++) {
+                arr[j][i % 2] = 0;
+                if (j +1 <= 2*k)
+                    arr[j][i % 2] += arr[j + 1][(i - 1) % 2];
+                if(j - 1 >= 0)
+                    arr[j][i % 2] += arr[j - 1][(i - 1) % 2];
+
+                arr[j][i % 2] = arr[j][i % 2] % mod;
+            }
+        }
+        return arr[k + pos][(i - 1) % 2];
+    }
+    public int numberOfWays(int startPos, int endPos, int k) {
+        return numberOfWaysIterative(startPos, endPos, k);
+        // int diff = Math.abs(startPos - endPos);
+        // if(diff > k) {
+        //     return 0;
+        // }
+        // dp = new int[2 * k + 1][k + 1];
+        // return numberOfWaysRecursive(k, k + diff, k);
+    }
+    public int numberOfWaysRecursive(int startPos, int endPos, int k) {
+        if(k < 0) {
+            return 0;
+        }
+        if(dp[startPos][k] > 0) {
+            return dp[startPos][k];
+        }
+        if(startPos == endPos && k == 0) {
+            return 1;
+        }
+        dp[startPos][k] =  numberOfWaysRecursive(startPos + 1, endPos, k - 1) + numberOfWaysRecursive(startPos - 1, endPos, k - 1);
+        return dp[startPos][k];
+    }
+}
