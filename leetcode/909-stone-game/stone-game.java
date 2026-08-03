64f51c2e8b031467592f85f36db55ff25d3990a6
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-08-03 12:39:37 +0200

    Time: 0 ms (100.00%) | Memory: 43.2 MB (37.02%) - LeetSync

--- leetcode/909-stone-game/stone-game.java
+++ leetcode/909-stone-game/stone-game.java
@@ -10,18 +10,11 @@ class Solution {
     
     */
     public boolean stoneGame(int[] piles) {
-        // return true;
-        // int total = 0;
-        // for(int i = 0; i < piles.length; i++) {
-        //     total += piles[i];
-        // }
-        // int dp[][][] = new int[piles.length][piles.length][2];
-        // int sum = stone(piles, 0, piles.length -  1, 0, dp);
-        // System.out.println("max sum allice " + dp[0][piles.length -1][0]);
-        // return sum > (total - sum);
-        int dp[][] = new int[piles.length][piles.length];
-        int adv = stone(piles, 0, piles.length -  1, dp);
-        return adv > 0;
+        return true;
+       
+        // int dp[][] = new int[piles.length][piles.length];
+        // int adv = stone(piles, 0, piles.length -  1, dp);
+        // return adv > 0;
     }
 
     public int stone(int[] nums, int i, int j, int dp[][]) {
