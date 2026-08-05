c7075b19e363bff38a5f48bbce435738e00f90e6
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-08-06 01:16:09 +0200

    Time: 309 ms (5.79%) | Memory: 48.5 MB (5.16%) - LeetSync

---
+++ leetcode/130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,65 @@
+class Solution {
+    public void solve(char[][] board) {
+        
+        // loop every cell
+        // take a visited array 
+        // move horizontally or vertically
+        int m = board.length;
+        int n = board[0].length;
+        int[][] visited = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                char c = board[i][j];
+                if(visited[i][j] == 1 || c == 'X')
+                    continue;
+                List<Integer> indices = bfs(i, j, board, visited);
+                if(indices != null) {
+                    for(Integer idx : indices) {
+                        board[idx / n][idx % n] = 'X';
+                    }
+                }
+            }
+        }
+    }
+
+    List<Integer> bfs(int i, int j, char[][] board, int[][] visited) {
+        visited[i][j] = 1;
+        int m = board.length;
+        int n = board[0].length;
+        boolean isValid = true;
+        if((i == 0 || i == m - 1 || j == 0 || j == n - 1))
+            isValid = false;
+        Queue<Integer> queue = new LinkedList<>();
+        // we add cell index to queue ie i * m + j
+        queue.add(i * n + j);
+        int[][] moves = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+        List<Integer> indices = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int idx = queue.remove();
+            int a = idx / n;
+            int b = idx % n;
+            indices.add(idx);
+            // top
+            for(int[] move : moves) {
+                int newA = a + move[0];
+                int newB = b + move[1];
+                if(newA < 0 || newA >= m || newB < 0 || newB >= n) {
+                    continue;
+                }
+                System.out.println("new A " + newA + " newB " + newB + " val " + board[newA][newB]);
+                if(board[newA][newB] == 'O' && (newA == 0 || newA == m - 1 || newB == 0 || newB == n - 1)) {
+                    System.out.println(" is valid false");
+                    isValid = false;
+                }
+                if(board[newA][newB] == 'O' && visited[newA][newB] == 0) {
+                    queue.add(newA * n + newB);
+                    visited[newA][newB] = 1;
+                }
+            }
+        }
+        if(!isValid)
+            return null;
+        return indices;
+    }
+}
