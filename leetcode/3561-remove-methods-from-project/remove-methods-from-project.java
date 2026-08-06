6e0fd0e0fd6e71a150db7655d5ae151bf8346140
AkashDeep Singh <57340046+AkashDeepSinghJassal@users.noreply.github.com>
2026-08-06 13:59:08 +0200

    Time: 117 ms (25.45%) | Memory: 285.2 MB (54.55%) - LeetSync

---
+++ leetcode/3561-remove-methods-from-project/remove-methods-from-project.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        // given invocations convert to graph 
+
+        // build graph 
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int[] inOrder = new int[n];
+        for(int i = 0; i < invocations.length; i++) {
+            graph.get(invocations[i][0]).add(invocations[i][1]);
+            inOrder[invocations[i][1]]++;
+        }
+        boolean[] sus = new boolean[n];
+        // find sus
+        check(k, graph, sus, inOrder);
+        System.out.println("inorder " + Arrays.toString(inOrder));
+        boolean isSus = true;
+        for(int i = 0; i < n; i++) {
+            if(sus[i] && inOrder[i] > 0) {
+                isSus = false;
+                break;
+            }
+        }
+        System.out.println("is sus" + isSus + " sus " + Arrays.toString(sus));
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if(!isSus || !sus[i]) {
+                res.add(i);
+            }
+        }
+
+        return res;
+    }
+
+    public void check (int val, List<List<Integer>> graph, boolean[] vis, int[] inOrder) {
+        Queue<Integer> queue = new LinkedList<>();
+        vis[val] = true;
+        queue.add(val);
+        while(!queue.isEmpty()) {
+            int curr = queue.remove();
+
+            for(Integer child: graph.get(curr)) {
+                inOrder[child]--;
+                if(!vis[child]) {
+                    queue.add(child);
+                    vis[child] = true;
+                    
+                }
+            }
+        }
+    }
+
+}
